73d9e18ac8b8d27edd242ab5be55984bfed85b4a
CastleMY <490523604@qq.com>
2018-11-26 22:25:44 +0800

    导出配置文件格式

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import DefaultConfig from './config';
+import { Context } from 'koa';
 export default DefaultConfig;
+/**
+ * 配置文件结构
+ */
+export interface CastleContext extends Context {
+    config: DefaultConfig;
+}
